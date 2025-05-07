756891dc7b51527c8bb6b0281bc20b4805a96189
abdullah-aydogan <abdullahaydogan245@outlook.com>
2025-05-07 10:19:30 +0300

    add API resources enum class

--- src/test/java/tr/abdullah/resources/APIResources.java
+++ src/test/java/tr/abdullah/resources/APIResources.java
@@ -2,5 +2,18 @@ package tr.abdullah.resources;
 
 public enum APIResources {
 
+    GetPlace("/maps/api/place/get/json"),
+    AddPlace("/maps/api/place/add/json"),
+    UpdatePlace("/maps/api/place/update/json"),
+    DeletePlace("/maps/api/place/delete/json");
 
+    private String resource;
+
+    APIResources(String resource) {
+        this.resource = resource;
+    }
+
+    public String getResource() {
+        return resource;
+    }
 }
